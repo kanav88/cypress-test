274ee05e7031c80516f3143feff2d0e901876f6e
kanav chadha <chadha.kanav@gmail.com>
2020-10-14 09:25:05 +1300

    added cypress code

---
+++ cypress/todomvc-tests/cypress/integration/todomvc.spec.js
@@ -0,0 +1,40 @@
+/// <reference types ="cypress"/>
+
+import { TodoPage } from "../page-objects/todo-page"
+
+describe('todo actions', () => {
+
+    const todoPage = new TodoPage
+    beforeEach(() =>{
+        todoPage.navigate()
+
+        todoPage.addTodo('clean room')
+
+    })
+
+    it('should add a new todo to the list', ()=>{
+        
+    
+        todoPage.validateTodoTxt(0, 'Clean room')
+    
+        cy.get('.toggle').should('not.be.checked')
+
+    })
+
+    it('should mark a todo as completed', ()=>{
+        cy.get('.toggle').click()
+
+        cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+
+    })
+
+    it('should clear completed todos', ()=>{
+        
+        cy.get('.toggle').click()
+        cy.contains('Clear completed').click()
+
+        cy.get('.todo-list').should('not.have.descendants', 'li')
+    })
+
+})
+
